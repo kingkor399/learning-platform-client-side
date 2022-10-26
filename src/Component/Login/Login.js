561e6c644131ef4dcd892e6d56ee8b860bae241d
kingkor399 <farhanrahmanyou@gmail.com>
2022-10-26 17:57:09 +0600

    second commit

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 const Login = () => {
     return (
-        <div>
-            <h4>This is Login</h4>
+        <div className='w-50 mx-auto border border-info mt-2 p-2 rounded'>
+            <h3 className='text-warning'>Please Login!!</h3>
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="success" type="submit">
+                Submit
+            </Button>
+        </Form>
         </div>
     );
 };
--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,9 +1,39 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 const Register = () => {
     return (
-        <div>
-            <h4>This is Register</h4>
+        <div className='w-50 mx-auto border border-info mt-2 p-2 rounded'>
+            <h3 className='text-warning'>Please Login!!</h3>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                <Form.Label>Photo URL</Form.Label>
+                <Form.Control type="text" name='photoURL' placeholder="Photo URL" required />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" name='confirm' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="success" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
